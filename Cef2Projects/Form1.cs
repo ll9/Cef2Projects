69fc9460e1cc8f7572ab9de7b1418f0775ce68da
ll9 <llwierer@web.de>
2019-02-18 19:34:40 +0100

    First Cef Project

--- Cef2Projects/Form1.cs
+++ Cef2Projects/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,30 @@ namespace Cef2Projects
 {
     public partial class Form1 : Form
     {
+        public ChromiumWebBrowser chromeBrowser;
+
         public Form1()
         {
             InitializeComponent();
+            // Start the browser after initialize global component
+            InitializeChromium();
+        }
+
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            chromeBrowser = new ChromiumWebBrowser("http://ourcodeworld.com");
+            // Add it to the form and fill it to the form window.
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
         }
     }
 }
